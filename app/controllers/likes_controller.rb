2ab23d34a9aa1f7670d9d240b4900ff0f07295aa
SergeyKolenko <venty80@gmail.com>
2014-08-20 11:08:34 +0300

    Likes without redirect

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
   def create
-    target = Likeable.find_by_resource_id(params[:resource_name], params[:resource_id])
-    current_user.like!(target)
-    redirect_to :back
+    @target = Likeable.find_by_resource_id(params[:resource_name], params[:resource_id])
+    current_user.like!(@target)
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+  end
   end
 
   def destroy
-    target = Likeable.find_by_resource_id(params[:resource_name], params[:resource_id])
-    current_user.unlike!(target)
-    redirect_to :back
+    @target = Likeable.find_by_resource_id(params[:resource_name], params[:resource_id])
+    current_user.unlike!(@target)
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
   end
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,12 +1,12 @@
 module LikesHelper
   def like_link_for(target)
-     link_to  like_path(:resource_id => target.id,:resource_name => target.class), :method => :post do
+     link_to  like_path(:resource_id => target.id,:resource_name => target.class), :method => :post, :remote => true do
                  image_tag "like-64x64.png", :size => "20", :alt => "Like", :title => "Like"
      end
   end
 
   def unlike_link_for(target)
-     link_to  unlike_path(:resource_id => target.id,:resource_name => target.class), :method => :delete  do
+     link_to  unlike_path(:resource_id => target.id,:resource_name => target.class), :method => :delete, :remote => true  do
                  image_tag "unlike-64x64.png", :size => "20", :alt => "Unlike", :title => "Unlike"
      end
   end
